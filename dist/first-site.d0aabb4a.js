34af9916809fd7575d9ebacf3ce6ef57ccab0633
SalmanHussein004 <salmanhu004@gmail.com>
2025-05-25 06:39:16 -0500

    Clean up: removed build and cache folders

--- dist/first-site.d0aabb4a.js
+++
@@ -1,2 +0,0 @@
-let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
-//# sourceMappingURL=first-site.d0aabb4a.js.map
