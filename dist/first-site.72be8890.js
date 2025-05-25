4cd29f783942141994a3f03eb86b58fdfc3e7d3a
SalmanHussein004 <salmanhu004@gmail.com>
2025-05-25 06:37:05 -0500

    Initial commit

---
+++ dist/first-site.72be8890.js
@@ -0,0 +1,6 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item)=>item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded");
+    }));
+
+//# sourceMappingURL=first-site.72be8890.js.map
---
+++ dist/first-site.d0aabb4a.js
@@ -0,0 +1,2 @@
+let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
+//# sourceMappingURL=first-site.d0aabb4a.js.map
